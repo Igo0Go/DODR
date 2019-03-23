d23023db01373fd19a6ad5c404ed263ccedd422f
DESKTOP-6F8Q8M6\Менем <menem8888@gmail.com>
2019-03-23 17:59:42 +0700

    Перенос кода из монстра, адаптация, хотьба, стояние

--- Assets/Menem/Scripts/SuperBotControllerChanged.cs
+++ Assets/Menem/Scripts/SuperBotControllerChanged.cs
@@ -2,20 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
-using UnityEngine.Experimental.Rendering;
 
-public enum Version
+public enum EnemyState
 {
-    Standart,
-    Meele,
-    Range
+    Stay,
+    Attack,
+    Walk,
+    Fight
 }
-
-public class SuperBotController : MonoBehaviour
+public class SuperBotControllerChanged : MonoBehaviour
 {
+    
+    
     public Transform Target;
+    
     public Vector3 LastPosition;
     public Vector3 StandardPosition;
+    
     public float Distance; //дистанция до цели
     [Range(0,50)]
     public float RangePursuit; // радиус преследования/обнаружения
@@ -27,26 +30,31 @@ public class SuperBotController : MonoBehaviour
     public float AngleCamera; // угол обзора камеры
     public float anglecam; // угол между фронтом камеры и ботом
     public float Camshooting; // угол при котором можно сказать что игрок целится в нас
+    
     public NavMeshAgent NavAgent;
-
-    public GameObject NavObject;
-    public GameObject ShockTurret;
     
     public Camera MainCamera;
 
     public bool IsMeleeAttack;
+    public bool wait;
     
-
     public Version Version; //енум
     
+    public EnemyState State;
+
+    public Animator Anim;
+    
+    // Start is called before the first frame update
     void Start()
     {
-        NavAgent = NavObject.GetComponent<NavMeshAgent>();
+        NavAgent = GetComponent<NavMeshAgent>();
+        Anim = GetComponent<Animator>();
         LastPosition = new Vector3(1000f, 1000f, 1000f);
         StandardPosition = LastPosition;
+        wait = false;
     }
-    //реагирование на действия в зоне
-    
+
+    // Update is called once per frame
     void Update()
     {
         if (Target != null)
@@ -58,7 +66,8 @@ public class SuperBotController : MonoBehaviour
             Vector3 VectorCam = MainCamera.transform.position - transform.position;
             anglecam = Vector3.Angle(MainCamera.transform.forward, -VectorCam);
             RaycastHit hit;
-            Ray ray =new Ray(transform.position+Vector3.up, Target.transform.position - transform.position);
+            Ray ray =new Ray(transform.position, Target.transform.position - transform.position);
+            
 
             if (IsMeleeAttack)
             {
@@ -71,22 +80,31 @@ public class SuperBotController : MonoBehaviour
 
             if (Version == Version.Standart)
             {
+ 
+
+                
+                
 
                 if (Distance < RangePursuit && Distance > RangeAttack && anglevis < AngleVision &&
-                    Physics.Raycast(ray, out hit, RangePursuit) && hit.transform.gameObject == Target)
+                    Physics.Raycast(ray, out hit, RangePursuit) && hit.transform.gameObject == Target.gameObject ) // если он видит перса но не в дистанции атаки
                 {
-                    if (anglecam < Camshooting)
-                    {
-                        IsMeleeAttack = false;
-                    }
+                    
+                       
+                            if (anglecam < Camshooting)
+                            {
+                                IsMeleeAttack = false;
+                            }
+                            // код для бега
 
-                    NavAgent.destination = Target.position;
-                    LastPosition = Target.position;
-                    ShockTurret.GetComponent<TurretScriptController>().target = Target;
+                            StateSolver = EnemyState.Walk;
+                            //ShockTurret.GetComponent<TurretScriptController>().target = Target;
+                       
+                    
+
                 }
                 else if (Distance <= RangeAttack && anglevis < AngleVision &&
                          Physics.Raycast(ray, out hit, RangePursuit) &&
-                         hit.transform.gameObject == Target)
+                         hit.transform.gameObject == Target.gameObject)
                 {
                     if (!IsMeleeAttack && anglecam > AngleCamera)
                     {
@@ -95,9 +113,11 @@ public class SuperBotController : MonoBehaviour
                     }
                     else //
                     {
+                        //подготовка к стрельбе 
+                        // стрельба = карутин с ключом
                         LastPosition = Target.position;
                         NavAgent.destination = transform.position;
-                        ShockTurret.GetComponent<TurretScriptController>().target = Target;
+                        //ShockTurret.GetComponent<TurretScriptController>().target = Target;
                         Attack();
                         
                     }
@@ -106,12 +126,14 @@ public class SuperBotController : MonoBehaviour
                 else if (LastPosition != StandardPosition)
                 {
                     NavAgent.destination = LastPosition;
-                    ShockTurret.GetComponent<SuperTurretScriptController>().target = null;
+                    //ShockTurret.GetComponent<SuperTurretScriptController>().target = null;
                 }
                 else
                 {
                     NavAgent.destination = transform.position;
-                    ShockTurret.GetComponent<SuperTurretScriptController>().target = null;
+                    //стоп
+                    StateSolver = EnemyState.Stay;
+                    // ShockTurret.GetComponent<SuperTurretScriptController>().target = null;
                 }
             }
             else if (Version == Version.Meele)
@@ -122,7 +144,7 @@ public class SuperBotController : MonoBehaviour
                 {
                     NavAgent.destination = Target.position;
                     LastPosition = Target.position;
-                    ShockTurret.GetComponent<TurretScriptController>().target = Target;
+                   // ShockTurret.GetComponent<TurretScriptController>().target = Target;
                 }
                 else if (Distance <= RangeMeleeAttack && anglevis < AngleVision &&
                          Physics.Raycast(ray, out hit, RangePursuit))
@@ -133,12 +155,12 @@ public class SuperBotController : MonoBehaviour
                 else if (LastPosition != StandardPosition)
                 {
                     NavAgent.destination = LastPosition;
-                    ShockTurret.GetComponent<SuperTurretScriptController>().target = null;
+                   // ShockTurret.GetComponent<SuperTurretScriptController>().target = null;
                 }
                 else
                 {
                     NavAgent.destination = transform.position;
-                    ShockTurret.GetComponent<SuperTurretScriptController>().target = null;
+                   //ShockTurret.GetComponent<SuperTurretScriptController>().target = null;
                 }
                 
             }
@@ -150,33 +172,34 @@ public class SuperBotController : MonoBehaviour
                 {
             
                     NavAgent.destination = Target.position;
-                    ShockTurret.GetComponent<TurretScriptController>().target = Target;
+                   // ShockTurret.GetComponent<TurretScriptController>().target = Target;
                     LastPosition = Target.position;
 
                 }
                 else if (Distance <= RangeShoot && anglevis < AngleVision)
                 {
                     NavAgent.destination = transform.position;
-                    ShockTurret.GetComponent<TurretScriptController>().target = Target;
+                   // ShockTurret.GetComponent<TurretScriptController>().target = Target;
                     LastPosition = Target.position;
                     Attack();
                 }
                 else if (LastPosition != StandardPosition)
                 {
                     NavAgent.destination = LastPosition;
-                    ShockTurret.GetComponent<SuperTurretScriptController>().target = null;
+                   // ShockTurret.GetComponent<SuperTurretScriptController>().target = null;
                 }
                 else
                 {
                     NavAgent.destination = transform.position;
-                    ShockTurret.GetComponent<SuperTurretScriptController>().target = null;
+                   // ShockTurret.GetComponent<SuperTurretScriptController>().target = null;
                 }
             }
         } 
         
-       
+        
+        
     }
-
+    
     private void Attack()
     {
         if (IsMeleeAttack || Distance<= RangeMeleeAttack)
@@ -184,5 +207,66 @@ public class SuperBotController : MonoBehaviour
             
         }
 
+    }
+    
+   
+        public virtual EnemyState StateSolver
+        {
+            get
+            {
+                return State;
+            }
+
+            set
+            {
+                State = value;
+                switch (StateSolver)
+                {
+                    case EnemyState.Stay:
+                        if (!wait)
+                        {
+                            wait = true;  
+                            CaseMethod(false, UnityEngine.Random.Range(-1, 1.1f), -1, 0, Target.transform.position);
+                            StartCoroutine(GetRandomStayState());
+                        }
+                        break;
+
+                    case EnemyState.Walk:
+                        //PlayThisClip(audioSource, Moves[1]);
+                        Debug.Log("begiii");
+                        CaseMethod(true, 0, 1, 0, Target.transform.position);
+                        break;
+
+                    case EnemyState.Attack:
+                        //GetAttackDistance();
+                        //PlayThisClip(audioSource, Moves[0]);
+                        CaseMethod(false, 0, 0, 1/*attackType*/, Target.transform.position);
+                        break;
+                }
+            }
+        }
+    protected void CaseMethod(bool navAgentEnebled, float xstate, float ysate, int attack, Vector3 destenation)
+    {
+        if (navAgentEnebled)
+        {
+            NavAgent.destination = Target.position;
+            Debug.Log("begiiii");
+        }
+        else
+        {
+            NavAgent.destination = transform.position;
+        }
+
+        Anim.SetInteger("Attack", attack);
+        Anim.SetFloat("Xstate", xstate);
+        Anim.SetFloat("Ystate", ysate);
+    }
+    protected IEnumerator GetRandomStayState()
+    {
+        Anim.SetFloat("Ystate", -1);
+        Anim.SetFloat("Xstate", UnityEngine.Random.Range(-1, 1.1f));
+        //FindPlayers();
+        yield return new WaitForSeconds(2);
+        wait = false;
     }
 }
