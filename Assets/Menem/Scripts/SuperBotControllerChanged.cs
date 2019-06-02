ca9ccd18f572acd5a92de52b634df21a0a93fa97
DESKTOP-6F8Q8M6\Менем <menem8888@gmail.com>
2019-06-02 19:39:01 +0700

    "Почистил" код(нет). Распихал по регионам код в SuperBot. Баг неисправлен.

--- Assets/Menem/Scripts/SuperBotControllerChanged.cs
+++ Assets/Menem/Scripts/SuperBotControllerChanged.cs
@@ -9,7 +9,6 @@ public enum EnemyState
     Attack,
     Walk,
     Fight,
-    WaitAttack
 }
 
 [RequireComponent(typeof(CharacterController))]
@@ -19,9 +18,13 @@ public class SuperBotControllerChanged : MonoBehaviour, IAlive
 
     public Vector3 LastPosition;
     public Vector3 StandardPosition;
-    
+
     public AutoWeapon gun;
-    
+
+    public LayerMask ignoreMask;
+
+    [Range(0, 300)] public int health;
+
     public float Distance; //дистанция до цели
     [Range(0, 50)] public float RangePursuit; // радиус преследования/обнаружения
     [Range(0, 50)] public float RangeShoot; //радиус стрельбы
@@ -34,7 +37,7 @@ public class SuperBotControllerChanged : MonoBehaviour, IAlive
     public float anglecam; // угол между фронтом камеры и ботом
     public float Camshooting; // угол при котором можно сказать что игрок целится в нас
     public float AngleShhotingForBot;
-   
+
     public NavMeshAgent NavAgent;
 
     public Camera MainCamera;
@@ -52,18 +55,13 @@ public class SuperBotControllerChanged : MonoBehaviour, IAlive
 
     public GameObject Geosphere; // rename to EyeGeosphere
 
+    private CharacterController charController;
 
-	[Range(0,300)]
-    public int health;
-	private CharacterController charController;
-	public LayerMask ignoreMask;
-    
-	
-	
-	
+    #region Логика
+
     void Start()
     {
-		alive = true;
+        alive = true;
         NavAgent = GetComponent<NavMeshAgent>();
         Anim = GetComponent<Animator>();
         LastPosition = new Vector3(1000f, 1000f, 1000f);
@@ -71,85 +69,19 @@ public class SuperBotControllerChanged : MonoBehaviour, IAlive
         wait = false;
         AngleVisionStandart = AngleVision;
         AngleVisionAlert = 360f;
-		charController = GetComponent<CharacterController>();
+        charController = GetComponent<CharacterController>();
     }
 
     void Update()
     {
-		if(alive)
-		{
-			if (Target != null)
-			{
-				DistanceTP = Vector3.Distance(transform.position, Target.position);
-			}	
-		}
-        
-    }
-    protected virtual IEnumerator Alarm()////почему он не ждет двух секунд и отключает сразу?
-    {
-        
-        yield return new WaitForSeconds(2);
-        Alert = false;
-
-    }
-
-    private void Attack() // кооод
-    {
-        Quaternion look = Quaternion.LookRotation(Target.transform.position - transform.position);
-        float anglevis = Quaternion.Angle(transform.rotation, look); //угол между ботом и персом
-        if (anglevis > AngleShhotingForBot)
+        if (alive)
         {
-            Vector3 relativePos = Target.position - transform.position;
-            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(relativePos), Time.deltaTime * 5);
-        }
-        else
-        {
-            if (IsMeleeAttack || Distance <= RangeMeleeAttack)
+            if (Target != null)
             {
-            
-                if (Anim.GetBool("Shoot"))
-                {
-                    Anim.SetBool("Shoot",false);
-                }
-                NavAgent.destination = transform.position;
-                
-                    Anim.SetTrigger("Fight");
-                    Anim.SetFloat("Xstate",0);
-                    Anim.SetFloat("Ystate",-1);
-                    //State = EnemyState.Fight;
-                
-                
+                DistanceTP = Vector3.Distance(transform.position, Target.position);
             }
-            else
-            {
-               
-               
-               
-                    NavAgent.destination = transform.position;
-                    Anim.SetBool("Shoot", true);
-                
-
-                        
-                
-            } 
         }
-        
-
     }
-
-	#region Обработчики событий анимаций
-	
-    public void Shoot()
-    {
-        gun.shootDelegate();
-    }
-	
-	public void CorrecteShootPoint()
-	{
-		gun.shootPoint.LookAt(Target.position + Vector3.up);
-	}
-
-	#endregion
 
     public virtual EnemyState StateSolver
     {
@@ -164,48 +96,22 @@ public class SuperBotControllerChanged : MonoBehaviour, IAlive
                     if (!wait)
                     {
                         wait = true;
-                        CaseMethod(false, UnityEngine.Random.Range(-1, 1.1f), -1,  Target.transform.position);
+                        CaseMethod(false, UnityEngine.Random.Range(-1, 1.1f), -1, Target.transform.position);
                         StartCoroutine(GetRandomStayState());
                     }
+
                     break;
-                
+
                 case EnemyState.Walk:
                     CaseMethod(true, 0, 1, Target.transform.position);
                     break;
-                
+
                 case EnemyState.Fight:
                     Debug.Log("1234");
                     Anim.SetTrigger("Fight");
                     break;
-
-                    
             }
         }
-    }
-
-    protected void CaseMethod(bool navAgentEnebled, float xstate, float ysate,  Vector3 destenation) //сделать что то сдесь как либо
-    {
-        if (navAgentEnebled)
-        {
-            NavAgent.destination = Target.position;
-            
-        }
-        else
-        {
-            NavAgent.destination = transform.position;
-        }
-        
-        Anim.SetFloat("Xstate", xstate);
-        Anim.SetFloat("Ystate", ysate);
-    }
-
-    protected IEnumerator GetRandomStayState()
-    {
-        Anim.SetFloat("Ystate", -1);
-        Anim.SetFloat("Xstate", UnityEngine.Random.Range(-1, 1.1f));
-        //FindPlayers();
-        yield return new WaitForSeconds(2);
-        wait = false;
     }
 
     public virtual float DistanceTP
@@ -220,7 +126,8 @@ public class SuperBotControllerChanged : MonoBehaviour, IAlive
             Vector3 VectorCam = MainCamera.transform.position - transform.position;
             anglecam = Vector3.Angle(MainCamera.transform.forward, -VectorCam);
             RaycastHit hit;
-            Ray ray = new Ray(Geosphere.transform.position, (Target.transform.position + Vector3.up) - Geosphere.transform.position);
+            Ray ray = new Ray(Geosphere.transform.position,
+                (Target.transform.position + Vector3.up) - Geosphere.transform.position);
 
             if (IsMeleeAttack)
             {
@@ -239,12 +146,12 @@ public class SuperBotControllerChanged : MonoBehaviour, IAlive
             {
                 AngleVision = AngleVisionStandart;
             }
-            
+
             if (Version == Version.Standart)
             {
                 if (Distance < RangePursuit && Distance > RangeAttack && anglevis < AngleVision &&
                     Physics.Raycast(ray, out hit, RangePursuit, ignoreMask) &&
-                    hit.transform.gameObject == Target.gameObject) // если он видит перса но не в дистанции атаки
+                    hit.transform.gameObject == Target.gameObject) 
                 {
                     if (anglecam < Camshooting)
                     {
@@ -258,23 +165,21 @@ public class SuperBotControllerChanged : MonoBehaviour, IAlive
 
                     if (Anim.GetBool("Shoot"))
                     {
-                        Anim.SetBool("Shoot",false);
+                        Anim.SetBool("Shoot", false);
                     }
                     // переключалка опасности
-                    
+
                     NavAgent.destination = Target.transform.position;
                     StateSolver = EnemyState.Walk;
                     LastPosition = Target.position;
                 }
                 else if (Distance <= RangeAttack && anglevis < AngleVision &&
                          Physics.Raycast(ray, out hit, RangePursuit, ignoreMask) &&
-                         hit.transform.gameObject == Target.gameObject) // если в ренже атаки
+                         hit.transform.gameObject == Target.gameObject) 
                 {
-                    
                     if (!IsMeleeAttack && anglecam > AngleCamera) // переключатель
                     {
                         IsMeleeAttack = true;
-
                     }
                     else // код атаки
                     {
@@ -283,25 +188,22 @@ public class SuperBotControllerChanged : MonoBehaviour, IAlive
                         if (!Alert)
                         {
                             Alert = true;
-                            //StartCoroutine(Alarm()); куда бы его переместить? 
                             
                         }
-                        Attack(); // делать код к атаке полностью в этом методе
 
+                        Attack(); 
                     }
-
                 }
-                else if (LastPosition != StandardPosition )
+                else if (LastPosition != StandardPosition)
                 {
-                    
                     if (Anim.GetBool("Shoot"))
                     {
-                        Anim.SetBool("Shoot",false);
+                        Anim.SetBool("Shoot", false);
                     }
+
                     NavAgent.destination = LastPosition;
-                    if (LastPosition.x == transform.position.x &&LastPosition.z == transform.position.z)
+                    if (LastPosition.x == transform.position.x && LastPosition.z == transform.position.z)
                     {
-                        
                         LastPosition = StandardPosition;
                     }
                 }
@@ -309,15 +211,16 @@ public class SuperBotControllerChanged : MonoBehaviour, IAlive
                 {
                     if (Anim.GetBool("Shoot"))
                     {
-                        Anim.SetBool("Shoot",false);
+                        Anim.SetBool("Shoot", false);
                     }
+
                     if (!wait)
                     {
                         wait = true;
-                        CaseMethod(false, UnityEngine.Random.Range(-1, 1.1f), -1,  Target.transform.position);
+                        CaseMethod(false, UnityEngine.Random.Range(-1, 1.1f), -1, Target.transform.position);
                         StartCoroutine(GetRandomStayState());
                     }
-                    
+
                     NavAgent.destination = transform.position;
                     //стоп
                     //StateSolver = EnemyState.Stay;
@@ -349,7 +252,6 @@ public class SuperBotControllerChanged : MonoBehaviour, IAlive
                     NavAgent.destination = transform.position;
                     //ShockTurret.GetComponent<SuperTurretScriptController>().target = null;
                 }
-
             }
             else
             {
@@ -357,11 +259,9 @@ public class SuperBotControllerChanged : MonoBehaviour, IAlive
                 if (Distance < RangePursuit && Distance > RangeShoot && anglevis < AngleVision &&
                     Physics.Raycast(ray, out hit, RangePursuit, ignoreMask) && hit.transform.gameObject == Target)
                 {
-
                     NavAgent.destination = Target.position;
                     // ShockTurret.GetComponent<TurretScriptController>().target = Target;
                     LastPosition = Target.position;
-
                 }
                 else if (Distance <= RangeShoot && anglevis < AngleVision)
                 {
@@ -381,114 +281,173 @@ public class SuperBotControllerChanged : MonoBehaviour, IAlive
                     // ShockTurret.GetComponent<SuperTurretScriptController>().target = null;
                 }
             }
-            
-
         }
     }
 
-    #region Health Damage and Death
+    #endregion
 
-    //public virtual void GetDamage(float value)
-    //{
-      //  if (alive)
-        //{
-            //Alarm = true; надо подумать как интегрировать получение урона и стоит ли вообще
-          //  NavAgent.enabled = false;
-            //Health -= value;
-            //Anim.SetInteger("Damage", (int)value);
-            //Anim.SetTrigger("GetDamage");
-        //}
-    //}
-	#endregion
-	
-	#region IAlive
-	
-	public void GetDamage(int damage)
+    #region Поиск пути
+
+    protected void CaseMethod(bool navAgentEnebled, float xstate, float ysate, Vector3 destenation)
     {
-		if(alive)
-		{
-			if (Health > 0)
-            {
-				NavAgent.destination = transform.position;
-				Anim.SetTrigger("Damage");
-                Health -= damage;
-			}
-		}
-    }
-  
-	public void ShockEffect(float time)
-    {
-        
-    }
-	
-	public void Dead()
-    {
-        alive = false;
-        NavAgent.destination = transform.position;
-		Anim.SetInteger("Health", health);
-        Anim.SetTrigger("Damage");
-    }
-	
-	public void DestroyModel()
-	{
-		Destroy(gameObject);
-	}
-  
-	public float Health
-    {
-        get
+        if (navAgentEnebled)
         {
-            return health;
+            NavAgent.destination = Target.position;
         }
+        else
+        {
+            NavAgent.destination = transform.position;
+        }
+
+        Anim.SetFloat("Xstate", xstate);
+        Anim.SetFloat("Ystate", ysate);
+    }
+
+    protected IEnumerator GetRandomStayState()
+    {
+        Anim.SetFloat("Ystate", -1);
+        Anim.SetFloat("Xstate", UnityEngine.Random.Range(-1, 1.1f));
+        //FindPlayers();
+        yield return new WaitForSeconds(2);
+        wait = false;
+    }
+
+    #endregion
+
+    #region Урон
+
+    private void Attack() 
+    {
+        Quaternion look = Quaternion.LookRotation(Target.transform.position - transform.position);
+        float anglevis = Quaternion.Angle(transform.rotation, look); //угол между ботом и персом
+        if (anglevis > AngleShhotingForBot)
+        {
+            Vector3 relativePos = Target.position - transform.position;
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(relativePos),
+                Time.deltaTime * 5);
+        }
+        else
+        {
+            if (IsMeleeAttack || Distance <= RangeMeleeAttack)
+            {
+                if (Anim.GetBool("Shoot"))
+                {
+                    Anim.SetBool("Shoot", false);
+                }
+
+                NavAgent.destination = transform.position;
+
+                Anim.SetTrigger("Fight");
+                Anim.SetFloat("Xstate", 0);
+                Anim.SetFloat("Ystate", -1);
+                
+            }
+            else
+            {
+                NavAgent.destination = transform.position;
+                Anim.SetBool("Shoot", true);
+            }
+        }
+    }
+
+
+
+    #endregion
+
+    #region Раздражители
+
+    protected virtual IEnumerator Alarm()
+    {
+        yield return new WaitForSeconds(2);
+        Alert = false;
+    }
+
+    #region Получение урона
+
+    public void GetDamage(int damage)
+    {
+        if (alive)
+        {
+            if (Health > 0)
+            {
+                NavAgent.destination = transform.position;
+                Anim.SetTrigger("Damage");
+                Health -= damage;
+            }
+        }
+    }
+    
+    protected virtual void OnTriggerEnter(Collider other)
+    {
+        if (other.tag.Equals("Sword"))
+        {
+            GetDamage(5);
+        }
+    }
+
+
+
+    #endregion
+
+    #endregion
+
+    #region Потребности
+
+    public float Health
+    {
+        get { return health; }
 
         set
         {
-            health = (int)value;
+            health = (int) value;
             if (health <= 0)
             {
-				alive = false;
+                alive = false;
                 Dead();
             }
-			else
-			{
-				Anim.SetInteger("Health", health);
-			}
+            else
+            {
+                Anim.SetInteger("Health", health);
+            }
         }
     }
-  
-	#endregion
-  
-    //public virtual void Death()
-    //{
-      //  alive = false;
-       // NavAgent.enabled = false;
-        //Anim.SetTrigger("Dead");
-    //}
     
-    //public virtual float Health
-    //{
-      //  get { return HP; }
-
-        //set
-        //{
-          //  if (value <= 0 && alive)
-            //{
-              //  Death();
-                //alive = false;
-                //HP = 0;
-            //}
-
-            //HP = value;
-        //}
-    //}
-    
-
-
-    protected virtual void OnTriggerEnter(Collider other)
+    public void Dead()
     {
-		if(other.tag.Equals("Sword"))
-		{
-			GetDamage(5);
-		}			
+        alive = false;
+        NavAgent.destination = transform.position;
+        Anim.SetInteger("Health", health);
+        Anim.SetTrigger("Damage");
     }
+    #endregion
+
+
+    #region Обработчики событий анимаций
+
+    public void Shoot()
+    {
+        gun.shootDelegate();
+    }
+
+    public void CorrecteShootPoint()
+    {
+        gun.shootPoint.LookAt(Target.position + Vector3.up);
+    }
+
+    #endregion
+
+
+    #region IAlive
+
+    public void ShockEffect(float time)
+    {
+    }
+
+
+    public void DestroyModel()
+    {
+        Destroy(gameObject);
+    }
+
+    #endregion
 }
